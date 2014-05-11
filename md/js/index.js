058ea09a3e9e099f30b225d5572dc51109eb875b
ajaxhe <ajaxhe@gmail.com>
2014-05-11 21:16:56 +0800

    添加首页js特效，修改了footer的样式

--- md/js/index.js
+++ md/js/index.js
@@ -1,15 +1,18 @@
 (function ($) {
     $.fn.extend({
-        qmTabs: function (container) 
+
+        bm2Tabs: function (center_div_name) 
 		{	
-			var tabs = $(this).children().children(".tabs");
-			var center_div = $(this).children(".bottom-2-content");
+			//var tabs = $(this).children().children(".tabs");
+			var tabs = $(this).find(".tabs");
+			var center_div = $(this).children(center_div_name);
 			var right_div = $(this).children(".bottom-2-right");
 	
 			
 			$(tabs).children("li").each(function (index) {
                 $(this).click( function () {
-					$(this).parent().children().children("a").attr("class", "");
+					//$(this).parent().children().children("a").attr("class", "");
+					$(this).parent().children().children("a").removeClass("selected");
                     $(this).children("a").addClass('selected');
                     $(center_div).children('ul').hide().eq(index).show();
 					$(right_div).children('ul').hide().eq(index).show();
@@ -17,18 +20,30 @@
             });
 			
         },
-		qmTabInit: function(container, index)
+		bm2TabInit: function(center_div_name,index)
 		{
 			//alert("Welcome i.ndex=" + index);
-			var tabs = $(this).children().children(".tabs");
-			var center_div = $(this).children(".bottom-2-content");
+			var tabs = $(this).find(".tabs");
+			var center_div = $(this).children(center_div_name);
 			var right_div = $(this).children(".bottom-2-right");
 
-			$(tabs).children("li").eq(index).children("a").removeClass().addClass('selected');
+			$(tabs).children("li").eq(index).children("a").removeClass("selected").addClass('selected');
 			$(center_div).children('ul').hide();
 			$(center_div).children('ul').eq(index).show();
 			$(right_div).children('ul').hide();
 			$(right_div).children('ul').eq(index).show();
+		},
+		
+		itemHovered: function()
+		{
+			$(this).mouseover(function(){
+				$(this).addClass("hovered");
+				//$(this).find(".item-ext").show("normal");
+			});
+			$(this).mouseout(function(){
+				$(this).removeClass("hovered");
+				//$(this).find(".item-ext").hide("normal");
+			});	
 		}
     });
 })(jQuery);
